b59b1802e395ec7f799d4ce81d023c81f886e671
doctorpangloss <accounts@appmana.com>
2026-06-16 16:54:30 -0700

    gitignore + purge committed kernel build artifacts (.o/.ko/.cmd/.mod from OOT in-place builds)

--- drivers/net/thunderbolt/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_modules.order := {   echo thunderbolt_net.o; :; } > modules.order
--- drivers/net/thunderbolt/.thunderbolt_net.mod.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_thunderbolt_net.mod := printf '%s\n'   main.o trace.o | awk '!x[$$0]++ { print("./"$$0) }' > thunderbolt_net.mod
